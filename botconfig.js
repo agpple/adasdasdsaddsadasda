e09a539934917bdba9c3a1bd57de3ce8afaca4cf
Xiaozhong <60964295+agpple@users.noreply.github.com>
2022-07-03 20:03:14 +0800

    Add files via upload

--- botconfig.js
+++ botconfig.js
@@ -11,13 +11,13 @@ module.exports = {
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
-  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  "24/7": true, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Pikachu is cute", //A Secret like a password
   IconURL:
-    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+    "https://media.discordapp.net/attachments/888117557381464086/900268520049360916/698320321702264902.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
-  Permissions: 2205281600, //Bot Inviting Permissions
-  Website: process.env.Website || "https://example.com", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  Permissions: 8, //Bot Inviting Permissions
+  Website: process.env.Website || "https://adasdasdsaddsadasda.xiaozhong0627.repl.co", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
   //If you get invalid oauth, make sure on the discord developer page you set the oauth url to something like: https://example.com/api/callback.
   
   Presence: {
@@ -30,9 +30,9 @@ module.exports = {
   // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "lavalink.darrenofficial.com", //- The host name or IP of the lavalink server.
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "hisudhan", //- The password of the lavalink server.
+    host: "connect.freelavalink.ga", //- The host name or IP of the lavalink server.
+    port: 2000, // The port that lavalink is listening to. This must be a number!
+    pass: "www.freelavalink.ga", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
   },
 
